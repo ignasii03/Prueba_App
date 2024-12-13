4c6617b2d6233d5a86573f5a4ba0cfa4f4dda75f
ignasii03 <ignasisp10@gmail.com>
2024-12-13 19:02:09 +0100

    modified:   components/ScreenWrapper.jsx

--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const ScreenWrapper = () => {
+const ScreenWrapper = ({ children, bg }) => {
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 5 : 30;
+
   return (
-    <View>
-      <Text>ScreenWrapper</Text>
-    </View>
+    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>{children}</View>
   );
 };
 
