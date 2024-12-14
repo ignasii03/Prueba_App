16e32acf4505fb177203c121e67efca7802de166
ignasii03 <ignasisp10@gmail.com>
2024-12-14 11:15:08 +0100

    modified:   app/login.jsx 	new file:   components/BackButton.jsx

--- app/login.jsx
+++ app/login.jsx
@@ -1,17 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
-import Icon from "../assets/icons";
-
+import { wp, hp } from "../helper/common";
+import { StatusBar } from "expo-status-bar";
+import BackButton from "../components/BackButton";
+import { useRouter } from "expo-router";
+import { theme } from "../constants/theme";
 const Login = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper>
-      <Text>Login</Text>
-      <Icon name="home" />
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text,
+  },
+  form: {
+    gap: 25,
+  },
+});
