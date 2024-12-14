48055a57e40f0e91680dbb9eba1ad03e4dad5728
ignasii03 <ignasisp10@gmail.com>
2024-12-14 19:22:13 +0100

    modified:   app/(main)/profile.jsx 	new file:   components/Header.jsx

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -1,10 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import { useAuth } from "../../contexts/AuthContext";
+import { useRouter } from "expo-router";
+import Header from "../../components/Header";
 
 const Profile = () => {
+  const { user, setAuth } = useAuth();
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <ScreenWrapper bg="white">
+      <UserHeader user={user} router={router} />
+    </ScreenWrapper>
+  );
+};
+
+const UserHeader = (user, router) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View>
+        <Header title="Perfil" showBackButton={true} />
+      </View>
     </View>
   );
 };
