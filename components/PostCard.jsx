03ba480a8f2bf9b67aff33e97a6da4a3c4b57017
ignasii03 <ignasisp10@gmail.com>
2024-12-16 10:41:19 +0100

    modified:   components/PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { theme } from "../constants/theme";
+import { hp } from "../helper/common";
 
 const PostCard = ({ item, currentUser, router, hasShadow = true }) => {
+  const shadowStyles = {
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.06,
+    shadowRadius: 6,
+    elevation: 1,
+  };
+
   return (
     <View>
       <Text>PostCard</Text>
@@ -11,4 +23,68 @@ const PostCard = ({ item, currentUser, router, hasShadow = true }) => {
 
 export default PostCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+    marginBottom: 15,
+    norderRadius: theme.radius.xxl * 1.1,
+    borderCurve: "continuous",
+    padding: 10,
+    paddingVertical: 12,
+    backgroundColor: "white",
+    borderWidth: 0.5,
+    borderColor: theme.colors.gray,
+    shadowColor: "#000",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userInfo: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+  username: {
+    fontSize: hp(1.7),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textDark,
+  },
+  postTime: {
+    height: hp(1.4),
+    color: theme.colors.textLight,
+    fontWeight: theme.fonts.medium,
+  },
+  content: {
+    gap: 10,
+  },
+  postMedia: {
+    height: hp(40),
+    width: "100%",
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+  postBody: {
+    marginLeft: 5,
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  footerButton: {
+    marginLeft: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 18,
+  },
+  count: {
+    color: theme.colors.text,
+    fontSize: hp(1.8),
+  },
+});
